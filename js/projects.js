fca6f6a29189cfb06b4484bf32134946f659854e
WALNOR-ENGINEERING <walnor.engineering@gmail.com>
2026-05-17 19:18:25 +0200

    Add functionality to fetch and display projects
    Fetch and display project data from JSON file in project cards.

--- js/projects.js
+++ js/projects.js
@@ -1,1 +1,30 @@
+fetch('../data/projects-en.json')
+    .then(response => response.json())
+    .then(data => {
 
+        const container = document.getElementById('projects-container');
+
+        data.forEach(project => {
+
+            const card = document.createElement('div');
+            card.classList.add('project-card');
+
+            card.innerHTML = `
+                <img src="${project.image}">
+
+                <div style="padding:20px;">
+                    <h3>${project.title}</h3>
+                    <p>${project.description}</p>
+
+                    <br>
+
+                    <a href="${project.github}" target="_blank" class="primary-button">
+                        View Repository
+                    </a>
+                </div>
+            `;
+
+            container.appendChild(card);
+        });
+
+    });
