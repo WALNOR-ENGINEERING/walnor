d40f5ba61b0cc3263a34cc083221a5a1ccb8ff32
WALNOR-ENGINEERING <walnor.engineering@gmail.com>
2026-05-18 20:43:04 +0200

    Add toggleProject function to manage project states

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('WALNOR loaded');
+
+function toggleProject(el) {
+
+    const project = el.parentElement;
+
+    const all = document.querySelectorAll(".project");
+
+    all.forEach(p => {
+
+        if (p !== project) {
+            p.classList.remove("active");
+        }
+
+    });
+
+    project.classList.toggle("active");
+}
